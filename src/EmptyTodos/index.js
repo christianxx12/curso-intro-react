afe3517278d8dfad6ee5fb586d58016e1ffa9eb8
Christian Rafael Guado Geronimo <crguado@gmail.com>
2021-09-17 18:15:24 -0500

    TODOMachine finish

---
+++ src/EmptyTodos/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EmptyTodos.css";
+
+function EmptyTodos() {
+  return (
+    <div className="firstTodo">
+      <p>¡Crea tu primer TODO!</p>
+    </div>
+  );
+}
+
+export { EmptyTodos };
